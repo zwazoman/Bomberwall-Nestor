9035e7025250e6abf7a6ad8addded78cc22b0518
zwazoman <141416442+zwazoman@users.noreply.github.com>
2024-11-19 13:16:02 +0100

    update conditions et weights

--- Assets/_Scripts/StateMachine/BaseState.cs
+++ Assets/_Scripts/StateMachine/BaseState.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public abstract class BaseState
 {
+    //potential conditions
+    int health;
+    int playerHealth;
+    int bombPossessedCount;
+    int closestBombPickupDistance;
+    int playerDistance;
+    int bombDistance;
+
+    //weights
+    int healthWheight;
+    int playerHealthWeight;
+    int bombPossessedCountWeight;
+    int closestBombPickupDistanceWeight;
+    int playerDistanceWeight;
+    int bombDistanceWeight;
+
+    //outcomes
+
     public abstract void OnEnter();
     public abstract void OnExit();
     public abstract void Update();
